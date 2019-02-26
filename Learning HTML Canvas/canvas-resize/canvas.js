8765fd640b7fd87bbe2678e2795c091c5a471ea7
Kaio Rosa <kaiosrosa@gmail.com>
2019-02-26 17:18:33 -0300

    Add files via upload

---
+++ Learning HTML Canvas/canvas-resize/canvas.js
@@ -0,0 +1,9 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+var c = canvas.getContext('2d');
+
+console.log(canvas);

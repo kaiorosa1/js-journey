c497df7e6c20a12e8ddb98dd8eccbe7045ccc149
Kaio Rosa <kaiosrosa@gmail.com>
2019-03-05 18:54:52 -0300

    add blueprint for Person

--- Mini Project Canvas/canvas.js
+++ Mini Project Canvas/canvas.js
@@ -6,114 +6,21 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+function Person(){
+	// the person itself
+}
 
-// var mouse = {
-// 	x: undefined,
-// 	y: undefined
-// };
+function Head(){
+	//Person's head
+}
 
-// // min and max radius of the circles
-// var maxRadius = 40;
-// var minRadius = 3;
+function Body(){
+ // Person's body
+}
+function Arm(){
+	// Person's arm
+}
 
-// var colorArray = [
-// '#a82b25',
-// '#eabea0',
-// '#596c6a',
-// '#c0ee59',
-// '#f902cd',
-// '#36f895'
-// ];
-
-// window.addEventListener('mousemove', function(event){
-// 	mouse.x = event.x;
-// 	mouse.y = event.y;
-// });
-
-// window.addEventListener('resize', function(){
-// 	canvas.width = window.innerWidth;
-// 	canvas.height = window.innerHeight;
-// 	init();
-// });
-
-// function Circle(x, y, dx, dy, radius){
-// 	this.x = x;
-// 	this.y = y;
-// 	this.dx = dx;
-// 	this.dy = dy;
-// 	this.radius = radius;
-// 	this.minRadius = radius;
-// 	this.color = colorArray[Math.floor(Math.random()* colorArray.length)];
-// 	this.draw = function(){
-// 		c.beginPath();
-// 		c.arc(this.x, this.y, this.radius, 0 , Math.PI *2, false);
-// 		c.strokeStyle = 'blue';
-// 		c.stroke();
-// 		c.fillStyle = this.color;
-// 		c.fill();
-// 	}
-// 	this.update = function(){
-// 		if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
-// 			this.dx=-this.dx;
-// 		}
-// 		if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
-// 			this.dy=-this.dy;
-// 		}
-// 		this.x += this.dx;
-// 		this.y += this.dy;
-
-
-// 		// interactivity 
-// 		if(mouse.x - this.x < 50 && mouse.x - this.x > -50
-// 			&& mouse.y - this.y < 50 && mouse.y - this.y > -50 ){
-// 			if(this.radius < maxRadius){
-// 				this.radius += 1;
-// 			}
-			
-// 		}else if(this.radius > this.minRadius){
-
-// 			this.radius -= 1;
-// 		}
-// 		this.draw();
-
-// 	}
-// }
-
-
-// var circle = new Circle(200,200,3,3,30);
-
-
-// // var x  = Math.random() * innerWidth; 
-// // var y = Math.random() * innerHeight;
-// // var dx = (Math.random() - 0.5)* 8;
-// // var dy = (Math.random() - 0.5)* 8;
-// // var raidius = 30;
-
-// var circleArray = [];
-
-// function init(){
-// 	circleArray = [];
-// 	for (var i = 0; i < 300; i++) {
-// 		var radius = Math.random()*5 + 1 ;
-// 		var x  = Math.random() * (innerWidth - radius*2) + radius; 
-// 		var y = Math.random() * (innerHeight - radius*2) + radius;
-// 		var dx = (Math.random() - 0.5)* 4;
-// 		var dy = (Math.random() - 0.5)* 4;
-		
-// 		circleArray.push(new Circle(x,y,dx,dy,radius));
-// 	}
-
-// }
-// function animate(){
-// 	requestAnimationFrame(animate);
-// 	c.clearRect(0,0, innerWidth, innerHeight);
-
-// 	for (var i = 0; i < circleArray.length; i++) {
-// 		circleArray[i].update();
-// 	}
-
-// }
-
-// init();
-// animate();
-
+function Leg(){
+   // Person's leg 
+}
